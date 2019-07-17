4856bc2396404150d871007088e6e8539cfef653
Natalia Dokuchaeva <natdok85@gmail.com>
2019-07-17 14:16:45 +0300

    second commit menu names changed

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,12 +1,12 @@
-let element = document.getElementsByClassName('menu__item');
-for(let i = 0; i < element.length; i++) {
-    if(element[i].children.length > 1) {
-        let active = element[i].getElementsByClassName('menu_sub');
-        element[i].onclick = function() {
-          let arr = Array.from(active);        
-          arr[0].classList.add('menu_active');
-          return false;
-        };
+let parentMenu = document.getElementsByClassName('menu__item');
+
+for(let i = 0; i < parentMenu.length; i++) {
+    if(parentMenu[i].children.length > 1) {      
+      let subMenu = parentMenu[i].getElementsByClassName('menu_sub');
+      parentMenu[i].onclick = function() {                              
+        subMenu[0].classList.add('menu_active');
+        return false;
+      };
     };
 };
 
