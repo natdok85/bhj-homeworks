3006d53e41a82a41b49c628b666f14b4d367c5ce
Natalia Dokuchaeva <natdok85@gmail.com>
2019-07-17 07:17:39 +0300

    second commit countdown restart added

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -2,7 +2,8 @@ const addTime = function() {
   const timer = document.getElementById("timer");  
   timer.textContent -=1;
   if(timer.textContent < 0) {
-    alert("Вы победили в конкурсе!");    
+    alert("Вы победили в конкурсе!");  
+    timer.textContent = 59;
   };
 };
 
