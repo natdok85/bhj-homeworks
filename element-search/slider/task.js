b0c5d2acc558b1d9f4aad1c7a4791f242b561190
Natalia Dokuchaeva <natdok85@gmail.com>
2019-07-17 15:41:23 +0300

    second commit slider

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -2,23 +2,35 @@ let items = document.getElementsByClassName('slider__item');
 let allItems = Array.from(items);
 let itemsNumber = allItems.length;
 
-
 let right = document.getElementsByClassName('slider__arrow_next');
 let rightA = Array.from(right);
 let rightArrow = rightA[0];
 
 let i = 1;
-rightArrow.onclick = function() {       
-    allItems[i].classList.add('slider__item_active');        
+rightArrow.onclick = function() {     
+    if(i > itemsNumber) {
+        i = 0;
+    };       
+    allItems[i].classList.add('slider__item_active'); 
+    allItems[i-1].classList.remove('slider__item_active');          
     i++;        
 };
-
 
 let left = document.getElementsByClassName('slider__arrow_prev');
 let leftA = Array.from(left);
 let leftArrow = leftA[0];
+
+let index = itemsNumber-1;
 leftArrow.onclick = function() {
     
+    allItems[index].classList.remove('slider__item_active');
+    allItems[index-1].classList.add('slider__item_active');
+    index--;  
+    if(index < 0) {
+        index = itemsNumber;
+    } else if(index > itemsNumber) {
+        index = 0;
+    };      
 };
 
 
