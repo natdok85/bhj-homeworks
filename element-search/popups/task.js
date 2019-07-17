fa782f143e365c857a568e8d12fbcfe4dddf0eea
Natalia Dokuchaeva <natdok85@gmail.com>
2019-07-17 11:51:23 +0300

    second commit popups

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,22 +1,27 @@
 let element = document.getElementById('modal_main');
-
 element.classList.add('modal_active');
 
 let close = document.getElementsByClassName('modal__close');
-let arr = Array.from(close);
-let closeWindow = arr[0];
-closeWindow.onclick = function() {
-    let element1 = document.getElementById('modal_main');
-    element1.classList.remove('modal_active');
-};
+let closeFirstWindow = close[0];
+let window1 = document.getElementById('modal_main');
+closeFirstWindow.onclick = function() {    
+    window1.classList.remove('modal_active');
+}; //закрываем первое окно крестиком
 
 let success = document.getElementsByClassName('show-success');
-let arr2 = Array.from(success);
-let showSuccess = arr2[0];
+let showSuccess = success[0];
 showSuccess.onclick = function() {
     let elementSuccess = document.getElementById('modal_success');
     elementSuccess.classList.add('modal_active');
-};
+    window1.classList.remove('modal_active');
+}; //меняем окна, первое закрывается
+
+let closeSecondWindow = close[2];
+let window2 = document.getElementById('modal_success');
+closeSecondWindow.onclick = function() {
+    window2.classList.remove('modal_active');
+} //закрываем второе окно крестиком
+
 
 
 
