a70cb84ee43fad16fc2c6a45a2cfa4c676bcba17
Natalia Dokuchaeva <natdok85@gmail.com>
2019-07-20 09:44:04 +0300

    third commit mole-game

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -3,36 +3,42 @@ let deadNum = parseInt(deadCounter.textContent);
 const lostCounter = document.getElementById("lost");
 let lostNum = parseInt(lostCounter.textContent);
 
-const hole1 = document.getElementById("hole1");
-
-
-let getHole = (index) => {return  document.getElementById(`hole${index}`)}; 
-
-hole1.onclick = function() {
-    if(hole1.classList.contains('hole_has-mole')) {
-        deadNum +=1;
-        deadCounter.textContent = deadNum;
-    } else {
-        lostNum +=1;
-        lostCounter.textContent = lostNum;
+for(let index = 1; index < 10; index++) {
+    let getHole = function(index) {
+        return document.getElementById(`hole${index}`)
+    };
+    
+    let hole = getHole(index);
+   
+    hole.onclick = function() {
+        if(hole.classList.contains('hole_has-mole')) {
+            deadNum +=1;
+            deadCounter.textContent = deadNum;
+        } else {
+            lostNum +=1;
+            lostCounter.textContent = lostNum;
+        };
+    
+        if(lostNum >= 5) {
+            alert("Вы проиграли");
+            deadNum = 0;
+            deadCounter.textContent = 0;
+            lostNum = 0;
+            lostCounter.textContent = 0;
+            
+        };
+    
+        if(lostNum < 5 && deadNum == 10) {
+            alert("Вы выиграли");
+            deadNum = 0;
+            deadCounter.textContent = 0;
+            lostNum = 0;
+            lostCounter.textContent = 0;
+            
+        };
     };
 
-    if(lostNum >= 5) {
-        alert("Вы проиграли");
-        deadNum = 0;
-        deadCounter.textContent = 0;
-        lostNum = 0;
-        lostCounter.textContent = 0;
-        
-    };
+}
 
-    if(lostNum < 5 && deadNum == 10) {
-        alert("Вы выиграли");
-        deadNum = 0;
-        deadCounter.textContent = 0;
-        lostNum = 0;
-        lostCounter.textContent = 0;
-        
-    };
-};
+
 
