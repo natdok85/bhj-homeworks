a3f3011a7a3c0a7e6b264604dd43510e2275111d
Natalia Dokuchaeva <natdok85@gmail.com>
2019-10-20 09:49:21 +0300

    first commit menu

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,14 +1,41 @@
-let parentMenu = document.getElementsByClassName('menu__item');
 
-for(let i = 0; i < parentMenu.length; i++) {
-    if(parentMenu[i].children.length > 1) {      
-      let subMenu = parentMenu[i].getElementsByClassName('menu_sub');
-      parentMenu[i].onclick = function() {                              
-        subMenu[0].classList.add('menu_active');
-        return false;
-      };
+let subMenues = Array.from(document.querySelectorAll('.menu_sub'));
+
+for(let i = 0; i < subMenues.length; i++) {
+  let x = subMenues[i];
+  
+  x.parentElement.querySelector('.menu__link').onclick = function() {
+
+    function notX(value) {      
+      return value != x;
     };
-};
+    
+    let filteredActive = subMenues.filter(notX);    
+
+    for(let element of filteredActive) {
+      element.classList.remove('menu_active');
+    };
+        
+    if(x.classList.contains('menu_active')) {
+      x.classList.remove('menu_active');
+      return false;        
+      } 
+     
+        
+      x.classList.add('menu_active');      
+      return false;
+    }
+    }
+
+    
+
+    
+
+  
+
+
+
+
 
 
 
