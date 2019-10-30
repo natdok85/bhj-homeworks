e3deeb740be62d3701ce8b65b4bbab84e3f27283
Natalia Dokuchaeva <natdok85@gmail.com>
2019-10-30 07:37:03 +0300

    first commit ads

---
+++ dom/ads/task.js
@@ -0,0 +1,20 @@
+function rotate() {
+    let ads = Array.from(document.getElementsByClassName('rotator__case'));    
+    let active = document.querySelector('.rotator__case_active');
+    let parent = active.closest('.rotator__case');  
+    let number = ads.indexOf(parent); 
+    
+    if(number == ads.length -1) {
+        ads[0].classList.add('rotator__case_active');                
+    };
+    
+    active.classList.remove('rotator__case_active');  
+   
+    active.nextElementSibling.classList.add('rotator__case_active');  
+    
+}
+
+setInterval(rotate, 1000);
+
+
+
