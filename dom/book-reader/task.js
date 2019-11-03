c4501f8b22f5ddd09322bc7aa5afa1d97046575d
Natalia Dokuchaeva <natdok85@gmail.com>
2019-11-03 20:10:39 +0300

    second commit book-reader

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,27 +1,14 @@
 let buttons = Array.from(document.getElementsByClassName('font-size'));
 let page = document.querySelector('.book');
 
-buttons[0].onclick = function() {
-    buttons[1].classList.remove('font-size_active');
-    this.classList.add('font-size_active');
-    page.classList.add('font-size_small');    
-    return false;
+function onclick() {
+    buttons[1].classList.remove('font-size_active');    
+    this.classList.add('font-size_active');       
+    let size = this.dataset.size;
+    page.classList.add('font-size_'+size); 
+    
+};
 
+for(let button of buttons) {
+    button.addEventListener('click', onclick);    
 }
-
-buttons[2].onclick = function() {
-    buttons[1].classList.remove('font-size_active');
-    this.classList.add('font-size_active');
-    page.classList.add('font-size_big');
-    return false;
-}
-// function onclick() {
-//     buttons[1].classList.remove('font-size_active');
-//     let size = this.dataset.size;
-//     console.log(size);
-//     return false;
-// }
-
-// for(let button of buttons) {
-//     button.addEventListener('click', onclick);
-// }
