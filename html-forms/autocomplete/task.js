19e7140300bc73000669953da29d037a5a9363a5
Natalia Dokuchaeva <natdok85@gmail.com>
2019-11-04 15:29:00 +0300

    first commit autocomplete

--- html-forms/autocomplete/task.js
+++ html-forms/autocomplete/task.js
@@ -68,6 +68,23 @@ class Autocomplete {
   }
 
   getMatches( text ) {
+
+    let field = document.querySelector('.autocomplete__search');    
+    text = field.value;
+    let variants = Array.from(this.input.options);
+    
+    for(let i = 0; i < variants.length; i++) {
+      if(variants[i].text.includes(text)) {
+        return [
+          {
+            text: variants[i].text,
+            value: variants[i].value
+          }
+        ]
+        
+      }
+    }
+    
     /*
       TODO: этот метод нужно дописать
       text - фраза, которую вводят в поле поиска
@@ -81,12 +98,12 @@ class Autocomplete {
         value: 'Содержимое атрибута value'
       }
     */
-    return [
-      {
-        text: 'Чубакка',
-        value: '1'
-      }
-    ];
+    // return [
+    //   {
+    //     text: 'Чубакка',
+    //     value: '1'
+    //   }
+    // ];
   }
 }
 
