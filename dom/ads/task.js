b0c7871d6fa230ab5cdc6b7ed1a9b396d98d9f2f
Natalia Dokuchaeva <natdok85@gmail.com>
2019-11-07 07:08:54 +0300

    third commit ads

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -5,20 +5,16 @@ function rotate() {
     let parent = active.closest('.rotator__case');  
     let number = ads.indexOf(parent); 
     
-    if(number == ads.length -1) {
-        ads[0].classList.add('rotator__case_active');                
+    if(number === ads.length -1) {
+        ads[0].classList.toggle('rotator__case_active');                
     };
     
-    active.classList.remove('rotator__case_active');  
+    active.classList.toggle('rotator__case_active');  
    
-    active.nextElementSibling.classList.add('rotator__case_active');   
+    active.nextElementSibling.classList.toggle('rotator__case_active');   
     
-    active.style.color = active.dataset.color;
-   
+    active.nextElementSibling.style.color = active.nextElementSibling.dataset.color;       
     
-}
+};
 
 setInterval(rotate, 1000);
-
-
-
