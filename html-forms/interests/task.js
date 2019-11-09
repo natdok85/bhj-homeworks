de329620f4340203a84bc2a35929ef17384e81ab
Natalia Dokuchaeva <natdok85@gmail.com>
2019-11-09 11:57:04 +0300

    second commit interests

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,32 +1,24 @@
-let items = Array.from(document.querySelectorAll('.interest__check'));
+let interests = Array.from(document.querySelectorAll('.interest'));
 
-let food = items[0];
-let animals = items[3];
-food.addEventListener('change', e => {
-    food.checked === true;    
-   
-    if(food.checked === true) {
-        items[1].checked = true;
-        items[2].checked = true;
-    } else {
-        items[1].checked = false;
-        items[2].checked = false;
+for(let interest of interests) {
+    interest.addEventListener('change', e => {
 
-    }
-})
+        let sub = event.target.closest('.interest');
+           
+        let subList = Array.from(sub.querySelectorAll('.interest'));
+        for(let item of subList) {
+            let checkbox = item.querySelector('.interest__check');
+            if(event.target.checked === true) {
+                checkbox.checked = true;                
+            } else {
+                checkbox.checked = false;
+            }
+        }        
+    
+    })
+}
 
-animals.addEventListener('change', e => {
-    animals.checked === true;    
-   
-    if(animals.checked === true) {
-        items[4].checked = true;
-        items[5].checked = true;
-    } else {
-        items[4].checked = false;
-        items[5].checked = false;
 
-    }
-})
 
 
 
