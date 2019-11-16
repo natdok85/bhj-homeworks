119fbae30d7cb989abaa91694d3c9bd0aac4087b
Natalia Dokuchaeva <natdok85@gmail.com>
2019-11-16 07:49:57 +0300

    second commit todo

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -4,9 +4,10 @@ let button = document.getElementById('tasks__add');
 
 clientInput.addEventListener('keydown', function(event) {
     if(event.keyCode === 13) {
-        if(this.value === '') {
+        if(this.value === '') {          
             return false;
         };
+        event.preventDefault();
         let tasks = document.createElement('div');
         tasks.innerHTML += 
     `<div class="task">
@@ -17,39 +18,10 @@ clientInput.addEventListener('keydown', function(event) {
     </div>`;
     tasksList.insertAdjacentElement('beforeend', tasks);
     clientInput.value = '';
-    let deleteButtons = Array.from(document.getElementsByClassName('task__remove'));
-for(let deleteButton of deleteButtons) {
-    deleteButton.addEventListener('click', event => {        
-        let parent = event.target.closest('.task');        
-        parent.remove();
-    })
-}
-    } 
-    
-})
-
-button.addEventListener('click', event => {
-    if(clientInput.value === '') {
-        return false;
-    };
-    let tasks = document.createElement('div');
-        tasks.innerHTML += 
-    `<div class="task">
-        <div class="task__title">
-            ${clientInput.value}
-        </div>
-        <a href="#" class="task__remove">&times;</a>
-    </div>`;
-    tasksList.insertAdjacentElement('beforeend', tasks);
-    clientInput.value = '';
-    
-    let deleteButtons = Array.from(document.getElementsByClassName('task__remove'));
-for(let deleteButton of deleteButtons) {
-    deleteButton.addEventListener('click', event => {        
-        let parent = event.target.closest('.task');        
-        parent.remove();
-    })
-}
+    tasks.querySelector('.task__remove').addEventListener('click', event => {
+        event.target.closest('.task').remove();
+    });
+    };       
 })
 
 
