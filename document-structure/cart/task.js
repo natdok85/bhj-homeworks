b39d8cffe23d30b0556430bab4750dbbdfee0a61
Natalia Dokuchaeva <natdok85@gmail.com>
2019-11-16 09:39:08 +0300

    third commit cart

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -7,15 +7,16 @@ caviarH3.insertAdjacentHTML('afterend', '<img src="https://ryboedov.ru/upload/ib
 //замена неработающей ссылки на картинку
 
 let addButtons = Array.from(document.querySelectorAll('.product__quantity-control_inc'));
-let deleteButtons = Array.from(document.querySelectorAll('.product__quantity-control_dec'));
+let decreaseButtons = Array.from(document.querySelectorAll('.product__quantity-control_dec'));
 let addItems = Array.from(document.querySelectorAll('.product__add'));
 let cartProduct = document.querySelector('.cart__products');
 let itemsCount = document.querySelectorAll('.cart__product-count');
+let counters = Array.from(document.querySelectorAll('.product__quantity-value'));
+
 
 for(let addButton of addButtons) {
     
-    addButton.addEventListener('click', event => {
-        let counters = Array.from(document.querySelectorAll('.product__quantity-value'));
+    addButton.addEventListener('click', event => {        
         if(addButton.closest('.product').dataset.id === '1') {
             number = parseInt(counters[0].textContent, 10);
             number++;
@@ -30,10 +31,9 @@ for(let addButton of addButtons) {
     })
 }
 
-for(let deleteButton of deleteButtons) {
-    deleteButton.addEventListener('click', event => {
-        let counters = Array.from(document.querySelectorAll('.product__quantity-value'));
-        if(deleteButton.closest('.product').dataset.id === '1') {
+for(let decreaseButton of decreaseButtons) {
+    decreaseButton.addEventListener('click', event => {       
+        if(decreaseButton.closest('.product').dataset.id === '1') {
             number = parseInt(counters[0].textContent, 10);
             if(number > 1) {
                 number--;
@@ -45,20 +45,40 @@ for(let deleteButton of deleteButtons) {
             number = parseInt(counters[1].textContent, 10);
             if(number > 1) {
                 number--;
-            counters[0].textContent = number; 
+            counters[1].textContent = number; 
             }     
 
         }
     })
 
 }
-
+let addedItems = [];
 for(let addItem of addItems) {
-    addItem.addEventListener('click', event => {
-        item = event.target.closest('.product');
+    addItem.addEventListener('click', event => {       
+
+        let item = event.target.closest('.product');
         let image = Array.from(item.getElementsByTagName('img'))[0].getAttribute('src');        
-        let count = item.getElementsByClassName('product__quantity-value')[0].textContent;        
-        cartProduct.insertAdjacentHTML('beforeend', `<div class="cart__product" data-id="${item.dataset.id}"><img class="cart__product-image" src=${image}><div class="cart__product-count">${count}</div></div>`);               
+        let count = item.getElementsByClassName('product__quantity-value')[0].textContent;     
+        let product = document.createElement('div');        
+        if(addedItems.includes(item.dataset.id)) {            
+            let cartElements = Array.from(cartProduct.querySelectorAll('.cart__product'));
+            for(let cartElement of cartElements) {
+                if(cartElement.dataset.id === item.dataset.id) {                    
+                    cartElement.querySelector('.cart__product-count').textContent = count;
+                }
+            }            
+
+        } else {
+            product.innerHTML += 
+            `<div class="cart__product" data-id="${item.dataset.id}">
+            <img class="cart__product-image" src=${image}>
+            <div class="cart__product-count">${count}</div>
+            </div>`;
+               
+            cartProduct.insertAdjacentElement('beforeend', product);  
+            addedItems.push(item.dataset.id);            
+
+        }       
        
     })
 }
