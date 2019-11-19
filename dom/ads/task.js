ab127415ebebaf67633a63cd64846bae6cd8af33
Natalia Dokuchaeva <natdok85@gmail.com>
2019-11-19 18:44:10 +0300

    fifth commit ads

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,9 +1,9 @@
 let ads = Array.from(document.getElementsByClassName('rotator__case'));
-let changeColor = function() {       
-    ads[0].style.color = ads[0].dataset.color;
-};
+// let changeColor = function() {       
+//     ads[0].style.color = ads[0].dataset.color;
+// };
 
-document.addEventListener("DOMContentLoaded", changeColor);
+// document.addEventListener("DOMContentLoaded", changeColor);
 
 function rotate() {       
     let active = document.querySelector('.rotator__case_active');
@@ -13,13 +13,14 @@ function rotate() {
         ads[0].classList.add('rotator__case_active');           
                   
     };
-    ads[ads.length-1].classList.remove('rotator__case_active');    
+
+    ads[ads.length-1].classList.remove('rotator__case_active');        
    
     active.classList.toggle('rotator__case_active');  
    
     active.nextElementSibling.classList.toggle('rotator__case_active');     
     
-    active.nextElementSibling.style.color = active.nextElementSibling.dataset.color;      
+    // active.nextElementSibling.style.color = active.nextElementSibling.dataset.color;      
      
     
 };
