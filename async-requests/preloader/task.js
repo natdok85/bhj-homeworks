a36e6c1d6bb410cc40013f781b2160ef46d77bc0
Natalia Dokuchaeva <natdok85@gmail.com>
2019-11-20 16:54:13 +0300

    first commit preloader

---
+++ async-requests/preloader/task.js
@@ -0,0 +1,38 @@
+let xhr = new XMLHttpRequest();
+
+xhr.open('GET', ' https://netology-slow-rest.herokuapp.com');
+xhr.send();
+
+xhr.addEventListener('readystatechange', event => {
+    if(xhr.readyState === xhr.DONE && xhr.status === 200) {
+        let loader = document.getElementById('loader');
+        loader.classList.remove('loader_active');
+        
+        let data = JSON.parse(xhr.responseText);
+        let currencies = data.response.Valute;
+        let values = Array.from(Object.values(currencies));
+        
+        let item = document.querySelector('.item');
+        for(let i = 0; i < values.length; i++) {
+            item.insertAdjacentHTML('beforeend', `          
+            <div class="item__code">
+                ${values[i].CharCode}
+            </div>
+            <div class="item__value">
+                ${values[i].Value}
+            </div>
+            <div class="item__currency">
+                руб.
+            </div>
+            
+            `);
+
+        };
+        
+
+       
+    
+        
+
+        };
+    });
